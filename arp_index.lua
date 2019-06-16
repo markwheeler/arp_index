1a0e4860afc8280e9952d5251dff08c6d9771ba1
Mark Wheeler <mark@markwheeler.net>
2019-06-16 16:05:53 +0100

    working using curl

--- arp_index.lua
+++ arp_index.lua
@@ -1,25 +1,74 @@
 -- The Arp Index
+-- 1.0.0 @markeats
+-- llllllll.co/t/arp-index
 --
 -- xxxxxxxxxx.
 --
--- v1.0.0
--- Mark Eats
+-- E1 : Xxxxxx
 --
 
 local ControlSpec = require "controlspec"
 local Graph = require "graph"
 local MusicUtil = require "musicutil"
 local Passersby = require "passersby/lib/passersby_engine"
--- local Socket = require "socket"
--- local Http = require "socket.http"
 
 engine.name = "Passersby"
 
 local SCREEN_FRAMERATE = 15
 local screen_dirty = true
 
-local stock_graph = nil
+local current_range = 1
+local ranges = {"1m", "3m", "1y"}
+local range_names = {"1 month", "3 months", "1 year"}
 
+local price_history
+local current_price
+local price_change
+
+
+local stock_graph
+
+
+local function get_stock_price_json(symbol, range)
+  range = range or "1m"
+  local token = "pk_f33c104ac1674f268ddb10ed18012c33"
+  local data = util.os_capture( "curl -s https://cloud.iexapis.com/stable/stock/" .. symbol .. "/chart/" .. range .. "?token=" .. token .. "&chartCloseOnly=true")
+  return data
+end
+
+local function process_stock_price_json(json)
+  
+  price_history = {}
+  current_price = nil
+  price_change = nil
+  
+  for entry in string.gmatch(json, "{(.-)}") do
+    local closing_price = tonumber(string.match(entry, "\"close\":([%d.-]+)"))
+    table.insert(price_history, closing_price)
+    current_price = closing_price
+    price_change = tonumber(string.match(entry, "\"change\":([%d.-]+)"))
+  end
+  
+end
+
+local function update_graph()
+  
+  stock_graph:remove_all_points()
+  
+  local min_price, max_price = 9999, 0
+  local num_prices = #price_history
+  
+  for i = 1, num_prices do
+    stock_graph:add_point(i, price_history[i])
+    min_price = math.min(min_price, price_history[i])
+    max_price = math.max(max_price, price_history[i])
+  end
+  
+  stock_graph:set_x_max(num_prices)
+  stock_graph:set_y_min(min_price)
+  stock_graph:set_y_max(max_price)
+  
+end
 
 -- Encoder input
 function enc(n, delta)
@@ -36,9 +85,20 @@ function key(n, z)
   if z == 1 then
     if n == 2 then
       
+      current_range = current_range % #ranges + 1
+      
     elseif n == 3 then
       
+      local data = get_stock_price_json("aapl", ranges[current_range])
+      -- print(data)
+      process_stock_price_json(data)
+      
+      update_graph()
+      
+      
     end
+    
+    screen_dirty = true
   end
 end
 
@@ -47,15 +107,9 @@ function init()
   
   Passersby.add_params()
   
-  -- print(Socket._VERSION)
-  
-  stock_graph = Graph.new(0, 6, "lin", 0, 100, "lin", "line", false, false)
+  stock_graph = Graph.new(1, 10, "lin", 0, 100, "lin", "line", false, false)
   stock_graph:set_position_and_size(3, 23, 122, 38)
   stock_graph:set_active(false)
-  
-  for i = 0, 14 do
-    stock_graph:add_point(util.linlin(0, 14, 0, 6, i), math.random(0, 100))
-  end
   
   local screen_refresh_metro = metro.init()
   screen_refresh_metro.event = function()
@@ -78,12 +132,17 @@ function redraw()
   screen.move(3, 9)
   screen.level(15)
   screen.text("AAPL")
-  screen.move(3, 18)
+  
   screen.level(3)
-  screen.text("157.14 +0.91")
+  if current_price and price_change then
+    screen.move(3, 18)
+    local price_change_string = price_change
+    if price_change > 0 then price_change_string = "+" .. price_change_string end
+    screen.text(current_price .. " " .. price_change_string)
+  end
   
   screen.move(125, 9)
-  screen.text_right("1 week")
+  screen.text_right(range_names[current_range])
   
   
   screen.fill()
